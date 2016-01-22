2fc76f37030542d1321846df59066c522dac16a9
James Harris <wopiancitizen123@gmail.com>
2016-01-22 01:49:54 +0000

    Update screenshot_generator.rb

--- lib/screenshot_generator.rb
+++ lib/screenshot_generator.rb
@@ -4,13 +4,13 @@ class ScreenshotGenerator < ::Middleman::Extension
   def initialize(app, options_hash={}, &block)
     super
     app.before_build do |builder|
-      builder.say "==> Theme thumbnail generator started", :pink
+      builder.say "== Theme thumbnail generator started", :green
       Screenshot.prepare
 
       themes.each do |theme|
         Screenshot.new(theme).generate_theme_image(builder)
       end
-      builder.say "==> Theme thumbnail generator finished", :lightgreen
+      builder.say "== Theme thumbnail generator finished", :green
     end
   end
 end
