98d4605fc4b06b9cfdf26cccb7234f4c8ed9c98e
James Harris <wopian@wopian.me>
2020-01-15 10:53:13 +0000

    chore: replace Google Analytics with non-tracking Goatcounter (#177)
    * chore: remove google analytics tag
    
    * chore: remove google analytics gem
    
    * chore: remove google analytics config
    
    * Update layout.erb
    
    * chore: update gemfile.lock
    
    * .
    
    * .
    
    * .
    
    * ..
    
    * ...
    
    * ignore js_errors
    
    * remove extra colon
    
    * .
    
    * ci: use installed ruby version

--- config.rb
+++ config.rb
@@ -3,55 +3,10 @@ require 'ostruct'
 require 'dotenv'
 Dotenv.load
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
-
-  activate :google_analytics do |ga|
-    ga.disable = true
-  end
 end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :css_dir, 'stylesheets'
 
@@ -71,13 +26,6 @@ configure :build do
 
   # Use relative URLs
   activate :relative_assets
-
-  activate :google_analytics do |ga|
-    # Property ID (default = nil)
-    ga.tracking_id = ENV['ANALYTICS_ID'] || 'TEST_GA_TRACKING_ID'
-    ga.enhanced_link_attribution = true
-    ga.minify = true
-  end
 end
 
 activate :deploy do |deploy|
